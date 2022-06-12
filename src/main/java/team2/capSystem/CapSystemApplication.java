bb73ff75e48e5d0830107ce9df9f1b603e64e39f
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-06-12 11:41:54 +0800

    first commit

---
+++ src/main/java/team2/capSystem/CapSystemApplication.java
@@ -0,0 +1,13 @@
+package team2.capSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team2/capSystem/CapSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package team2.capSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
