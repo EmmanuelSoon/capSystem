a7f73077a0e1bb7b3aa55dd08f264726c342d818
KRISH <KRISH@LAPTOP-E7E5F6HB.mshome.net>
2022-06-12 12:01:25 +0800

    Testing Push and commit.

--- src/main/java/team2/capSystem/CapSystemApplication.java
+++ src/main/java/team2/capSystem/CapSystemApplication.java
@@ -8,6 +8,8 @@ public class CapSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapSystemApplication.class, args);
+		
+		//Checking push and commit
 	}
 
 }
