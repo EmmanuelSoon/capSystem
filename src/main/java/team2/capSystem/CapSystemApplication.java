8e2064059c50df072f75a1591f57c537da79cdbc
alylim <lim.alywx@gmail.com>
2022-06-12 12:07:48 +0800

    Testing commit on this branch

--- src/main/java/team2/capSystem/CapSystemApplication.java
+++ src/main/java/team2/capSystem/CapSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CapSystemApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello world!");
 		SpringApplication.run(CapSystemApplication.class, args);
 	}
 
