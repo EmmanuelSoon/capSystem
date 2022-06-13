89a779afb696ea95eafade59171e12d8b0ef288a
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-06-13 22:15:20 +0800

    test folder

--- src/main/java/team2/capSystem/model/CourseDetail.java
+++ src/main/java/team2/capSystem/model/CourseDetail.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.sql.DataTruncation;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
---
+++ src/test/java/team2/capSystem/CRUDtests/CourseTest.java
@@ -0,0 +1,5 @@
+package team2.capSystem.CRUDtests;
+
+public class CourseTest {
+    
+}
