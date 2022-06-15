51a4564359362f6ba34ef7883093ed121ca286bc
alylim <lim.alywx@gmail.com>
2022-06-15 13:26:30 +0800

    Implementing student controller with view pages

--- src/main/java/team2/capSystem/controller/StudentController.java
+++ src/main/java/team2/capSystem/controller/StudentController.java
@@ -3,7 +3,9 @@ package team2.capSystem.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import team2.capSystem.repo.StudentRepository;
 
@@ -12,10 +14,22 @@ import team2.capSystem.repo.StudentRepository;
 public class StudentController {
     
     @Autowired StudentRepository sRepo;
-
+    
     @GetMapping("/")
     public String showDashboard(){
-        return "/student-dashboard";
+        return "student-dashboard";
     }
+
+    @RequestMapping("/course-history")
+    public String showCourseHistory(){
+        return "student-course";
+    }
+
+    @RequestMapping("/enroll")
+    public String showAvailbleCourses(){
+        return "student-enroll-course";
+    }
+
+
 
 }
