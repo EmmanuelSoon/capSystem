c806b8f693a340bc626aa16f6b26e6e3990be9d1
Gaw Yan Heng <e0941670@u.nus.edu>
2022-06-16 23:32:41 +0800

    Added controllers while revising, gathered information for service layer logic and queries

--- src/main/java/team2/capSystem/controller/LecturerController.java
+++ src/main/java/team2/capSystem/controller/LecturerController.java
@@ -2,6 +2,8 @@ package team2.capSystem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import team2.capSystem.services.CourseService;
@@ -22,8 +24,26 @@ public class LecturerController {
 
     @RequestMapping(value="/dashboard")
     public String displayDashboard() {
-        return "/lecturer/lecturerDashboard";
+        return "/lecturer/lecturer-dashboard";
     }
     
+    @RequestMapping(value="/course-taught")
+    public String viewCoursetaught() {
+    	return "/lecturer/lecturer-course-taught";
+    }
+    
+    @RequestMapping(value="/course-enrolment")
+    public String viewCourseEnrol() {
+    	return "/lecturer/lecturer-view-enrolment";
+    }
+    @RequestMapping(value="/grading")
+    public String gradecourse() {
+    	return "/lecturer/lecturer-grade-course";
+    }
+ 
+    @RequestMapping(value="/student-performance")
+    public String viewStudentPerformance() {
+    	return "/lecturer/lecturer-course-taught";
+    }
     
 }
