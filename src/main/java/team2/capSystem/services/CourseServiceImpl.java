2cf4397ff580bab530ce72f72e0d3644129916d7
Emman <reach.emmanuelsoon@gmail.com>
2022-06-16 11:45:41 +0800

    course service implement file

---
+++ src/main/java/team2/capSystem/services/CourseServiceImpl.java
@@ -0,0 +1,26 @@
+package team2.capSystem.services;
+
+import java.time.LocalDate;
+import team2.capSystem.model.*;
+
+
+public class CourseServiceImpl implements CourseService {
+    
+    public void createCourse(String name, String desc){
+
+    };
+
+    public void updateCourse(Course course, String change){
+
+    };
+
+    public void getCourse(int id){
+
+     };
+
+
+    public void addCourseDetail(Course course, LocalDate start, LocalDate end){
+
+    };
+
+}
