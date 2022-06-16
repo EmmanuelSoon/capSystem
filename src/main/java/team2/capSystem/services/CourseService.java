c12a926d36fc48df9104d76a839c2483132c125f
Emman <reach.emmanuelsoon@gmail.com>
2022-06-16 12:00:09 +0800

    addCourseDetail & CreateCourseDetail

--- src/main/java/team2/capSystem/services/CourseService.java
+++ src/main/java/team2/capSystem/services/CourseService.java
@@ -1,15 +1,19 @@
 package team2.capSystem.services;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import team2.capSystem.model.Course;
+import team2.capSystem.model.CourseDetail;
+import team2.capSystem.model.Lecturer;
 
 public interface CourseService {
     void createCourse(String name, String desc);
     void updateCourse(Course course, String change);
     void getCourse(int id);
 
-    void addCourseDetail(Course course, LocalDate start, LocalDate end);
+    void addCourseDetail(CourseDetail courseDetail, List<Lecturer> lectList);
+    CourseDetail createCourseDetail(Course course, LocalDate start, LocalDate end);
 
 
 }
