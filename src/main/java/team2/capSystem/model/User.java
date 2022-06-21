b6b61900d5b6ba23e3a6119e4b5669c0d5d8bdf5
Hein Lin Zaw <e0941685@u.nus.edu>
2022-06-21 14:08:17 +0800

    add password hashing

--- src/main/java/team2/capSystem/model/User.java
+++ src/main/java/team2/capSystem/model/User.java
@@ -2,31 +2,39 @@ package team2.capSystem.model;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+<<<<<<< Updated upstream
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+=======
+>>>>>>> Stashed changes
 import javax.validation.constraints.NotEmpty;
 
-import lombok.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @MappedSuperclass
 @NoArgsConstructor
 @Data
 public class User {
 	@Column(unique = true)
-	@NotEmpty
-	@NotBlank
+	@NotEmpty(message = "Username cannot be empty.")
     protected String username;
-	@NotEmpty
-	@NotBlank
+	@NotEmpty(message = "Password cannot be empty.")
     protected String password;
     protected String name;
     @Email
     protected String email;
     protected Boolean active = true;
-
+       
+    
     public User(String username, String password, String name, String email) {
         this.username = username;
-        this.password = password;
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(password);
+        this.password = encodedPassword;
         this.name = name;
         this.email = email;
     }
