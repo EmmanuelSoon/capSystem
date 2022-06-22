6d3639200e86332b27a3ecfe7765d51f7cdf1771
alylim <lim.alywx@gmail.com>
2022-06-22 20:46:19 +0800

    accidentally added in a function previously removed

--- src/main/java/team2/capSystem/controller/StudentController.java
+++ src/main/java/team2/capSystem/controller/StudentController.java
@@ -225,12 +225,6 @@ public class StudentController {
     	
         
     }
-
-    @RequestMapping("/exception")
-    public String exceptionPage(Model model){
-        model.addAttribute("error", "testing the error page");
-        return "students/handleExceptionPage";
-    }
         
     //helper functions
     
