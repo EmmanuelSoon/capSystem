a9a2d7fda58e37e9eea4de2cf3a12d57b9fd5529
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-06-22 09:29:31 +0800

    seeding more course details

--- src/main/java/team2/capSystem/services/SeedDBServiceImpl.java
+++ src/main/java/team2/capSystem/services/SeedDBServiceImpl.java
@@ -91,12 +91,20 @@ public class SeedDBServiceImpl implements SeedDBService{
 
 
             //Seeding of CourseDetails
+
+
             CourseDetail cook1 = courseService.createCourseDetail(cookingCourse, LocalDate.of(2021, 06, 15), LocalDate.of(2022, 06, 15));
             CourseDetail cook2 = courseService.createCourseDetail(cookingCourse, LocalDate.of(2022, 06, 15), LocalDate.of(2023, 06, 15));
+            CourseDetail cook3 = courseService.createCourseDetail(cookingCourse, LocalDate.of(2020, 06, 15), LocalDate.of(2021, 06, 15));
+            CourseDetail cook4 = courseService.createCourseDetail(cookingCourse, LocalDate.of(2023, 06, 15), LocalDate.of(2024, 06, 15));
 
             CourseDetail jap1 = courseService.createCourseDetail(japCourse, LocalDate.of(2022, 1, 01), LocalDate.of(2022, 3, 30));
             CourseDetail jap2 = courseService.createCourseDetail(japCourse, LocalDate.of(2022, 4, 01), LocalDate.of(2022, 7, 30));
             CourseDetail jap3 = courseService.createCourseDetail(japCourse, LocalDate.of(2022, 8, 01), LocalDate.of(2022, 11, 30));
+            CourseDetail jap4 = courseService.createCourseDetail(japCourse, LocalDate.of(2021, 1, 01), LocalDate.of(2021, 3, 30));
+            CourseDetail jap5 = courseService.createCourseDetail(japCourse, LocalDate.of(2021, 4, 01), LocalDate.of(2021, 7, 30));
+            CourseDetail jap6 = courseService.createCourseDetail(japCourse, LocalDate.of(2021, 8, 01), LocalDate.of(2021, 11, 30));
+            CourseDetail jap7 = courseService.createCourseDetail(japCourse, LocalDate.of(2023, 1, 01), LocalDate.of(2023, 3, 30));
 
 
             CourseDetail java1 = courseService.createCourseDetail(javaCourse, LocalDate.of(2022, 01, 01), LocalDate.of(2022, 02, 28));
@@ -109,7 +117,10 @@ public class SeedDBServiceImpl implements SeedDBService{
 
             CourseDetail fopcs1 = courseService.createCourseDetail(fopcs, LocalDate.of(2022, 01, 01), LocalDate.of(2022, 06, 30));
             CourseDetail fopcs2 = courseService.createCourseDetail(fopcs, LocalDate.of(2022, 07, 01), LocalDate.of(2022, 12, 30));
-
+            CourseDetail fopcs3 = courseService.createCourseDetail(fopcs, LocalDate.of(2023, 01, 01), LocalDate.of(2023, 06, 30));
+            CourseDetail fopcs4 = courseService.createCourseDetail(fopcs, LocalDate.of(2023, 07, 01), LocalDate.of(2023, 12, 30));
+            CourseDetail fopcs5 = courseService.createCourseDetail(fopcs, LocalDate.of(2021, 01, 01), LocalDate.of(2021, 06, 30));
+            CourseDetail fopcs6 = courseService.createCourseDetail(fopcs, LocalDate.of(2021, 07, 01), LocalDate.of(2021, 12, 30));
 
             CourseDetail mobile1 = courseService.createCourseDetail(mobileCourse, LocalDate.of(2022, 01, 01), LocalDate.of(2022, 01, 15));
             CourseDetail mobile2 = courseService.createCourseDetail(mobileCourse, LocalDate.of(2022, 03, 01), LocalDate.of(2022, 3, 15));
@@ -121,27 +132,43 @@ public class SeedDBServiceImpl implements SeedDBService{
 
             CourseDetail pokemon1 = courseService.createCourseDetail(pokemonCourse, LocalDate.of(2022, 1, 01), LocalDate.of(2022, 3, 30));
             CourseDetail pokemon2 = courseService.createCourseDetail(pokemonCourse, LocalDate.of(2022, 6, 01), LocalDate.of(2022, 9, 30));
+            CourseDetail pokemon3 = courseService.createCourseDetail(pokemonCourse, LocalDate.of(2023, 1, 01), LocalDate.of(2023, 3, 30));
+            CourseDetail pokemon4 = courseService.createCourseDetail(pokemonCourse, LocalDate.of(2023, 6, 01), LocalDate.of(2023, 9, 30));
+            CourseDetail pokemon5 = courseService.createCourseDetail(pokemonCourse, LocalDate.of(2021, 1, 01), LocalDate.of(2021, 3, 30));
+            CourseDetail pokemon6 = courseService.createCourseDetail(pokemonCourse, LocalDate.of(2021, 6, 01), LocalDate.of(2021, 9, 30));
 
             CourseDetail farming1 = courseService.createCourseDetail(farmingCourse, LocalDate.of(2021, 1, 01), LocalDate.of(2021, 12, 30));
             CourseDetail farming2 = courseService.createCourseDetail(farmingCourse, LocalDate.of(2022, 1, 01), LocalDate.of(2022, 12, 30));
             CourseDetail farming3 = courseService.createCourseDetail(farmingCourse, LocalDate.of(2023, 1, 01), LocalDate.of(2023, 12, 30));
+            CourseDetail farming4 = courseService.createCourseDetail(farmingCourse, LocalDate.of(2020, 1, 01), LocalDate.of(2020, 12, 30));
 
 
             CourseDetail ml1 = courseService.createCourseDetail(mlCourse, LocalDate.of(2021, 9, 01), LocalDate.of(2022, 3, 30));
             CourseDetail ml2 = courseService.createCourseDetail(mlCourse, LocalDate.of(2022, 4, 01), LocalDate.of(2022, 8, 30));
             CourseDetail ml3 = courseService.createCourseDetail(mlCourse, LocalDate.of(2022, 9, 01), LocalDate.of(2023, 3, 30));
+            CourseDetail ml4 = courseService.createCourseDetail(mlCourse, LocalDate.of(2021, 4, 01), LocalDate.of(2021, 8, 30));
+            CourseDetail ml5 = courseService.createCourseDetail(mlCourse, LocalDate.of(2023, 4, 01), LocalDate.of(2023, 8, 30));
+            CourseDetail ml6 = courseService.createCourseDetail(mlCourse, LocalDate.of(2023, 9, 01), LocalDate.of(2024, 3, 30));
 
             
             //Association Lecturers with courses 
             courseService.addLecturer(cook1, liufan);
             courseService.addLecturer(cook2, liufan);
+            courseService.addLecturer(cook3, liufan);
+            courseService.addLecturer(cook4, liufan);
             courseService.addLecturer(cook1, suria);
             courseService.addLecturer(cook2, suria);
-
+            courseService.addLecturer(cook3, suria);
+            courseService.addLecturer(cook4, suria);
 
             courseService.addLecturer(jap1, tsukiji);
             courseService.addLecturer(jap2, tsukiji);
             courseService.addLecturer(jap3, tsukiji);
+            courseService.addLecturer(jap4, tsukiji);
+            courseService.addLecturer(jap5, tsukiji);
+            courseService.addLecturer(jap6, tsukiji);
+            courseService.addLecturer(jap7, tsukiji);
+
 
             courseService.addLecturer(java1, tin);
             courseService.addLecturer(java1, suria);
@@ -162,6 +189,18 @@ public class SeedDBServiceImpl implements SeedDBService{
             courseService.addLecturer(fopcs2, tin);
             courseService.addLecturer(fopcs1, cherwah);
             courseService.addLecturer(fopcs2, cherwah);
+            courseService.addLecturer(fopcs3, liufan);
+            courseService.addLecturer(fopcs4, liufan);
+            courseService.addLecturer(fopcs3, tin);
+            courseService.addLecturer(fopcs4, tin);
+            courseService.addLecturer(fopcs3, cherwah);
+            courseService.addLecturer(fopcs4, cherwah);
+            courseService.addLecturer(fopcs5, liufan);
+            courseService.addLecturer(fopcs6, liufan);
+            courseService.addLecturer(fopcs5, tin);
+            courseService.addLecturer(fopcs6, tin);
+            courseService.addLecturer(fopcs5, cherwah);
+            courseService.addLecturer(fopcs6, cherwah);
 
             courseService.addLecturer(mobile1, cherwah);
             courseService.addLecturer(mobile1, esther);
@@ -178,10 +217,20 @@ public class SeedDBServiceImpl implements SeedDBService{
             courseService.addLecturer(pokemon1, yeunkwan);
             courseService.addLecturer(pokemon2, tsukiji);
             courseService.addLecturer(pokemon2, yeunkwan);
+            courseService.addLecturer(pokemon3, tsukiji);
+            courseService.addLecturer(pokemon3, yeunkwan);
+            courseService.addLecturer(pokemon4, tsukiji);
+            courseService.addLecturer(pokemon4, yeunkwan);
+            courseService.addLecturer(pokemon5, tsukiji);
+            courseService.addLecturer(pokemon5, yeunkwan);
+            courseService.addLecturer(pokemon6, tsukiji);
+            courseService.addLecturer(pokemon6, yeunkwan);
 
             courseService.addLecturer(farming1, suria);
             courseService.addLecturer(farming2, suria);
             courseService.addLecturer(farming3, esther);
+            courseService.addLecturer(farming4, suria);
+
 
             courseService.addLecturer(ml1, cherwah);
             courseService.addLecturer(ml1, yeunkwan);
@@ -189,6 +238,12 @@ public class SeedDBServiceImpl implements SeedDBService{
             courseService.addLecturer(ml2, yeunkwan);
             courseService.addLecturer(ml3, cherwah);
             courseService.addLecturer(ml3, yeunkwan);
+            courseService.addLecturer(ml4, cherwah);
+            courseService.addLecturer(ml4, yeunkwan);
+            courseService.addLecturer(ml5, cherwah);
+            courseService.addLecturer(ml5, yeunkwan);
+            courseService.addLecturer(ml6, cherwah);
+            courseService.addLecturer(ml6, yeunkwan);
 
         }
     }
@@ -196,6 +251,7 @@ public class SeedDBServiceImpl implements SeedDBService{
     public void loadStudentCourseData(){
         if(!studentService.scTableExist()){
             //Students 
+            Student student = studentService.findStudentByUsername("Student1");
             Student emmanuel = studentService.findStudentByUsername("emmanuel");
             Student alyssa = studentService.findStudentByUsername("alyssa");
             Student gavin = studentService.findStudentByUsername("gavin");
@@ -215,21 +271,64 @@ public class SeedDBServiceImpl implements SeedDBService{
             Course mlCourse = courseService.getCourseByName("ML");				
             
             //Course Details 
-            CourseDetail jap1 = courseService.findExactCourseDetail(japCourse, LocalDate.of(2022, 1, 01), LocalDate.of(2022, 3, 30));
             CourseDetail cook1 = courseService.findExactCourseDetail(cookingCourse, LocalDate.of(2021, 06, 15), LocalDate.of(2022, 06, 15));
-            CourseDetail pokemon1 = courseService.findExactCourseDetail(pokemonCourse, LocalDate.of(2022, 1, 01), LocalDate.of(2022, 3, 30));
-            CourseDetail farming1 = courseService.findExactCourseDetail(farmingCourse, LocalDate.of(2021, 1, 01), LocalDate.of(2021, 12, 30));
+            CourseDetail cook2 = courseService.findExactCourseDetail(cookingCourse, LocalDate.of(2022, 06, 15), LocalDate.of(2023, 06, 15));
+
+
+            CourseDetail jap1 = courseService.findExactCourseDetail(japCourse, LocalDate.of(2022, 1, 01), LocalDate.of(2022, 3, 30));
+            CourseDetail jap2 = courseService.findExactCourseDetail(japCourse, LocalDate.of(2022, 4, 01), LocalDate.of(2022, 7, 30));
+            CourseDetail jap3 = courseService.findExactCourseDetail(japCourse, LocalDate.of(2022, 8, 01), LocalDate.of(2022, 11, 30));
+
+
+            CourseDetail java1 = courseService.findExactCourseDetail(javaCourse, LocalDate.of(2022, 01, 01), LocalDate.of(2022, 02, 28));
+            CourseDetail java2 = courseService.findExactCourseDetail(javaCourse, LocalDate.of(2022, 03, 01), LocalDate.of(2022, 04, 30));
             CourseDetail java3 = courseService.findExactCourseDetail(javaCourse, LocalDate.of(2022, 05, 01), LocalDate.of(2022, 06, 30));
             CourseDetail java4 = courseService.findExactCourseDetail(javaCourse, LocalDate.of(2022, 07, 01), LocalDate.of(2022, 8, 30));
+            CourseDetail java5 = courseService.findExactCourseDetail(javaCourse, LocalDate.of(2022, 9, 01), LocalDate.of(2022, 10, 30));
+            CourseDetail java6 = courseService.findExactCourseDetail(javaCourse, LocalDate.of(2022, 11, 01), LocalDate.of(2023, 12, 30));
+
+
+            CourseDetail fopcs1 = courseService.findExactCourseDetail(fopcs, LocalDate.of(2022, 01, 01), LocalDate.of(2022, 06, 30));
+            CourseDetail fopcs2 = courseService.findExactCourseDetail(fopcs, LocalDate.of(2022, 07, 01), LocalDate.of(2022, 12, 30));
+
+
+            CourseDetail mobile1 = courseService.findExactCourseDetail(mobileCourse, LocalDate.of(2022, 01, 01), LocalDate.of(2022, 01, 15));
+            CourseDetail mobile2 = courseService.findExactCourseDetail(mobileCourse, LocalDate.of(2022, 03, 01), LocalDate.of(2022, 3, 15));
+            CourseDetail mobile3 = courseService.findExactCourseDetail(mobileCourse, LocalDate.of(2022, 06, 01), LocalDate.of(2022, 6, 15));
+            CourseDetail mobile4 = courseService.findExactCourseDetail(mobileCourse, LocalDate.of(2022, 9, 01), LocalDate.of(2022, 9, 15));
+            CourseDetail mobile5 = courseService.findExactCourseDetail(mobileCourse, LocalDate.of(2022, 12, 01), LocalDate.of(2022, 12, 15));
+
+
+
+            CourseDetail pokemon1 = courseService.findExactCourseDetail(pokemonCourse, LocalDate.of(2022, 1, 01), LocalDate.of(2022, 3, 30));
+            CourseDetail pokemon2 = courseService.findExactCourseDetail(pokemonCourse, LocalDate.of(2022, 6, 01), LocalDate.of(2022, 9, 30));
+
+            CourseDetail farming1 = courseService.findExactCourseDetail(farmingCourse, LocalDate.of(2021, 1, 01), LocalDate.of(2021, 12, 30));
+            CourseDetail farming2 = courseService.findExactCourseDetail(farmingCourse, LocalDate.of(2022, 1, 01), LocalDate.of(2022, 12, 30));
+            CourseDetail farming3 = courseService.findExactCourseDetail(farmingCourse, LocalDate.of(2023, 1, 01), LocalDate.of(2023, 12, 30));
+
+
+            CourseDetail ml1 = courseService.findExactCourseDetail(mlCourse, LocalDate.of(2021, 9, 01), LocalDate.of(2022, 3, 30));
+            CourseDetail ml2 = courseService.findExactCourseDetail(mlCourse, LocalDate.of(2022, 4, 01), LocalDate.of(2022, 8, 30));
+            CourseDetail ml3 = courseService.findExactCourseDetail(mlCourse, LocalDate.of(2022, 9, 01), LocalDate.of(2023, 3, 30));
+
+
 
             //Creating StudentCourses
-            courseService.addStudentToCourseDetail(jap1, emmanuel, 4);
-            courseService.addStudentToCourseDetail(jap1, alyssa, 4);
-            courseService.addStudentToCourseDetail(jap1, hein, 4);
+
 
             courseService.addStudentToCourseDetail(cook1, yc, 5);
             courseService.addStudentToCourseDetail(cook1, yoonmie, 4);
             courseService.addStudentToCourseDetail(cook1, gavin, 4.5);
+            courseService.addStudentToCourseDetail(cook2, alyssa, -1);
+            courseService.addStudentToCourseDetail(cook2, hein, -1);
+
+            courseService.addStudentToCourseDetail(jap1, emmanuel, 4);
+            courseService.addStudentToCourseDetail(jap1, alyssa, 4);
+            courseService.addStudentToCourseDetail(jap1, hein, 4);            
+            courseService.addStudentToCourseDetail(jap2, gavin, -1);
+            courseService.addStudentToCourseDetail(jap2, anand, -1);
+            
 
             courseService.addStudentToCourseDetail(pokemon1, emmanuel, 3);
             courseService.addStudentToCourseDetail(pokemon1, hein, 5);
@@ -241,8 +340,9 @@ public class SeedDBServiceImpl implements SeedDBService{
             courseService.addStudentToCourseDetail(farming1, hein, 2.5);
             courseService.addStudentToCourseDetail(farming1, yc, 5);
 
-            courseService.addStudentToCourseDetail(java3, alyssa, -1);
+            courseService.addStudentToCourseDetail(mobile3, yc, 5);
 
+            courseService.addStudentToCourseDetail(java3, alyssa, -1);
 
             courseService.addStudentToCourseDetail(java4, anand, -1);
             courseService.addStudentToCourseDetail(java4, yc, -1);
@@ -250,6 +350,8 @@ public class SeedDBServiceImpl implements SeedDBService{
             courseService.addStudentToCourseDetail(java4, gavin, -1);            
 
 
+            courseService.addStudentToCourseDetail(ml1, student, 5);
+            courseService.addStudentToCourseDetail(ml2, anand, -1);
 
 
         }
